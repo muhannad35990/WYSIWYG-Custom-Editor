6c933cd93538de6e8b78ccf4dc2e0e3b825ff9d6
Muhannad <muhannad35990@gmail.com>
2025-01-16 23:09:04 +0300

    add test

--- __tests__/ToolbarItem.test.js
+++ __tests__/ToolbarItem.test.js
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom"
 import { render, screen, fireEvent } from "@testing-library/react"
-import ToolbarItem from "../components/ToolbarItem"
+import ToolbarItem from "../src/components/ToolbarItem"
 import { EditorState } from "draft-js"
 
 // Mock the `setEditorState` function
@@ -10,7 +10,6 @@ const mockSetEditorState = jest.fn()
 const mockEditorState = EditorState.createEmpty()
 
 describe("ToolbarItem component", () => {
-  // Test 1: Should render the component correctly with given props
   it("should render the button with the given label", () => {
     render(
       <ToolbarItem
@@ -25,7 +24,6 @@ describe("ToolbarItem component", () => {
     expect(screen.getByText("Bold")).toBeInTheDocument()
   })
 
-  // Test 2: Should toggle the inline style when button is clicked
   it("should toggle inline style on button click", () => {
     render(
       <ToolbarItem
@@ -43,12 +41,8 @@ describe("ToolbarItem component", () => {
 
     // Check if setEditorState was called with the updated editorState
     expect(mockSetEditorState).toHaveBeenCalled()
-
-    // Since we are mocking the setEditorState, you may mock the behavior of RichUtils.toggleInlineStyle here
-    // For example, you can add a mock implementation that returns a new editorState with the 'BOLD' style.
   })
 
-  // Test 3: Should apply the active style when the inline style is active
   it("should apply active styles when inline style is active", () => {
     // Create an editor state with the 'BOLD' style applied
     const activeEditorState = EditorState.createWithContent(
@@ -74,10 +68,9 @@ describe("ToolbarItem component", () => {
 
     // Check if button has the active class applied
     expect(button).toHaveClass("bg-fuchsia-500 text-white")
-    expect(button).toHaveClass("activeStyle") // ToolbarItemActiveStyle should be applied
+    expect(button).toHaveClass("activeStyle")
   })
 
-  // Test 4: Should not apply active styles when inline style is not active
   it("should not apply active styles when inline style is not active", () => {
     render(
       <ToolbarItem
